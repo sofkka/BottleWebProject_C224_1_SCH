bc1e090dc86ab1b888466b7b5e051a5fd0465a36
Sofya Chernyshova <sof4ern@mail.ru>
2025-05-21 13:31:10 +0300

    Удалены комментарии на русском языке из файла routes.py

--- BottleWebProject_C224_1_SCH/routes.py
+++ BottleWebProject_C224_1_SCH/routes.py
@@ -71,34 +71,23 @@ def wolf_island():
 @route('/infection_spread')
 @view('module2_infection_spread')
 def module2_infection_spread():
-    # Извлекается параметр 'size' из GET-запроса, задающий размер сетки. Если параметр отсутствует - 9
     size = request.GET.get('size', '9')
 
-    # Извлекается параметр 'continue_from', содержащий JSON-представление сетки для продолжения симуляции.
-    # Если параметр отсутствует - None.
     continue_from = request.GET.get('continue_from')
 
-    # Проверяется параметр 'save', определяющий, нужно ли сохранить состояние сетки в JSON.
-    # Значение 'true' в запросе преобразуется в булево True, иначе False.
     save = request.GET.get('save', 'false') == 'true'
 
-    # Извлекается параметр 'step', указывающий текущий шаг симуляции. Если параметр отсутствует, используется 0.
     step = int(request.GET.get('step', '0'))
 
-    # Проверяется корректность параметра size и преобразуется в целое число.
     try:
-        # Параметр size преобразуется в целое число.
         size = int(size)
 
-        # Если size является чётным числом или выходит за допустимый диапазон (3–15) - size = 9.
         if size % 2 == 0 or size < 3 or size > 15:
             size = 9
 
-    # Обработка исключения ValueError, возникающее при некорректном формате size.
     except ValueError:
         size = 9
 
-    # Обработка запроса на сохранение
     if save:
         try:
             grid = json.loads(continue_from)
@@ -107,47 +96,32 @@ def module2_infection_spread():
         except Exception as e:
             return json.dumps({'success': False, 'message': str(e)})
 
-    # Загружается сетка для симуляции: либо продолжается из переданного состояния, либо создаётся новая.
     if continue_from:
-        # Проверяется возможность продолжения симуляции из переданного состояния.
         try:
-            # Параметр continue_from декодируется из JSON в двумерный список (сетку).
             continue_grid = json.loads(continue_from)
 
-        # Обработка исключения JSONDecodeError, возникающего при некорректном формате continue_from.
         except json.JSONDecodeError:
-            # Если декодирование не удалось, создаётся новая сетка размером size.
             grid = initialize_grid(size)
     else:
-        # Если continue_from отсутствует, создаётся новая сетка размером size.
         grid = initialize_grid(size)
 
-    # Подготовка данных для передачи в шаблон module2_infection_spread.tpl.
 
-    # Запускается симуляция распространения инфекции.
     simulation_steps, final_grid, all_healthy = simulate_all_steps(grid, size)
 
-    # Преобразуется каждый шаг симуляции в JSON-совместимый формат.
     simulation_steps_json = [grid_to_json(step) for step in simulation_steps]
 
-    # Преобразуется конечная сетка в JSON-совместимый формат.
     final_grid_json = grid_to_json(final_grid)
 
-    # Словарь для передачи.
     return dict(
         title='The model of ringworm infection spread',
         message='A simulation model exploring the spread of ringworm infection.',
         year=datetime.now().year,
-        # Передаётся начальный размер сетки.
         initial_size=size,
 
-        # Передаётся список шагов симуляции в JSON-формате для отображения в JavaScript.
         simulation_steps_json=simulation_steps_json,
 
-        # Передаётся конечная сетка в JSON-формате для продолжения или отображения.
         final_grid_json=final_grid_json,
 
-        # Передаётся флаг, указывающий, являются ли все ячейки здоровыми.
         all_healthy=all_healthy
     )
 
